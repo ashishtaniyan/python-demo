eabe66f87b5d38bf59e853650cec9cbd97561e70
aaniyan-verimatrix <ashish.aniyan@gmail.com>
2019-05-10 08:53:04 -0700

    Added Test with negetive and positive scenarios

--- test_lidar_scanner.py
+++ test_lidar_scanner.py
@@ -2,19 +2,64 @@ from lidar_scanner import *
 import pytest
 
 def test_range_filter():
-	a = np.array([[0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.], [2., 3., 4., 1., 0.], [3., 3., 3., 1., 3.],
-				[10., 2., 4., 0., 0.]])
+	a = [0., 1., 2., 1., 3.]
 	first_filter = Range_filter()
-	for i in a :
-		x = first_filter.update(i)
-	b = [[0.03, 1.0, 2.0, 1.0, 3.0], [1.0, 5.0, 5, 1.0, 3.0], [2.0, 3.0, 4.0, 1.0, 0.03], [3.0, 3.0, 3.0, 1.0, 3.0], [5, 2.0, 4.0, 0.03, 0.03]]
-	assert first_filter.update(a) == b, "Array did not match. Test failed"
+		#x = first_filter.update(a)
+	processed_data = first_filter.update(a)
+	b = [0.03, 1.0, 2.0, 1.0, 3.0]
+	assert  processed_data == b, "Array did not match. "
 
-def test_temporal_filter():
-	a = np.array([[0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.], [2., 3., 4., 1., 0.], [3., 3., 3., 1., 3.],
-				[10., 2., 4., 0., 0.]])
+def test_range_filter_max():
+	a = [51, 1., 2., 1., 3.]
+	first_filter = Range_filter()
+		#x = first_filter.update(a)
+	processed_data = first_filter.update(a)
+	b = [50, 1.0, 2.0, 1.0, 3.0]
+	assert  processed_data == b, "Array did not match. "
+
+def test_range_filter_supermax():
+	a = [51, 1., 2., 1., 3.,3000,9999]
+	first_filter = Range_filter()
+		#x = first_filter.update(a)
+	processed_data = first_filter.update(a)
+	b = [50, 1.0, 2.0, 1.0, 3.0, 50, 50]
+	assert  processed_data == b, "Array did not match. "
+
+def test_temporal_filter_positive():
+	a = [0., 1., 2., 1., 3.]
 	second_filter = Temporal_filter(3)
-	for i in b :
-		y = second_filter.update(i)
-	b = [[0, 1, 2, 1, 1.0, 3.0], [0.5, 3.0, 4.5, 1.0, 3.0], [1.0, 3.0, 4.0, 1.0, 3.0], [2.0, 3.0, 4.0, 1.0, 3.0], [3.0, 3.0, 4.0, 1.0, 0.0]]
-	assert b == y, "Array did not match. Test failed"
+	processed_data = second_filter.update(a)
+	b = [0., 1., 2., 1., 3.]
+	print(processed_data)
+	assert np.all(processed_data == b), "Array did not match."
+
+def test_temporal_filter_negetive():
+	a = [0., 1., 2., 1., 3.]
+	second_filter = Temporal_filter(3)
+	processed_data = second_filter.update(a)
+	b = [0., 1., 2., 1., 3.5]
+	print(processed_data)
+	assert not np.all(processed_data == b), "Array did not match."
+
+def show_result():
+	#Test Data Set	
+		a = np.array([[0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.], [2., 3., 4., 1., 0.], [3., 3., 3., 1., 3.],
+				[10., 2., 4., 0., 0.]])
+
+		b = np.array([[0., 1., 2., 1., 3.], [1., 5., 7., 1., 3.], [2., 3., 4., 1., 0.], [3., 3., 3., 1., 3.],
+				[10., 2., 4., 0., 0.]])
+
+		first_filter = Range_filter()
+		second_filter = Temporal_filter(3)
+		print("before ")
+		print(a)
+		print("After Range Filter")
+
+		for i in a :
+			x = first_filter.update(i)
+			print(x)
+
+		print("After Temporal Filter")
+		for i in b :
+			y = second_filter.update(i)
+			print(y)
